1a3fbd7aebaa5fd8118a001a8ef3141e8d3e8e77
JoaoRViana <joaorviana98@gmail.com>
2024-10-29 11:20:57 -0300

    feat: Create Account model

---
+++ src/main/java/com/example/dio_api/domain/model/Account.java
@@ -0,0 +1,67 @@
+package com.example.dio_api.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+
+    private String agency;
+
+    @Column(name = "Balance", scale = 2)
+    private BigDecimal balance;
+
+    @Column(name = "Limit", scale = 2)
+    private BigDecimal limit;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+    
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
